87e1abbe1e6a1069b00fa3d816bb45ce65f7972e
Siris <vincentcmv@gmail.com>
2021-12-17 22:08:59 +0530

    Finalizing

--- App.java
+++ App.java
@@ -3,18 +3,118 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.awt.*;    
+import javax.imageio.ImageIO;
 
+import java.awt.*;    
+import java.awt.event.*;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.util.Scanner;
 
 class ClientSecrets {
     static String apiKey = "9adfefb504af4f3d8ad182141211212";
 }
 
-class App extends Frame {    
+class APIException extends Exception {
+    int code;
+    APIException (String msg, int code) {
+        super(msg);
+        this.code = code;
+    }
+
+    public void printMessage(Frame f) {
+        if (this.code == 0) return;
+        
+        if (this.code == 401) Print.error("Invalid API Key", f);
+        else if (this.code == 400) Print.error("Invalid Location", f);
+        else if (this.code == 403) Print.error("API Key exhausted", f);
+    }
+}
+
+class Utils {
+    public static String getHourly (JSONArray data, String unit) {
+        String result = "Hourly Data: ";
+
+        try {
+            for (int i = 0; i < 5; ++i) {
+                JSONObject d = data.getJSONObject(i);
+                String time = d.getString("time");
+                String temp = Double.toString(unit == "°C"? d.getDouble("temp_c"): d.getDouble("temp_f"));
+                result = result + time.split(" ")[1] + "-->" + temp + (unit == "°C"? "°C": "°F") + " | ";
+            }
+            return result;
+        } catch (JSONException e) {
+            return result;
+        }
+    }
+}
+
+class Print {
+    public static void current (StringBuffer rawWeatherData, String unit, Frame frame) throws JSONException {
+        JSONObject weatherObj = new JSONObject(rawWeatherData.toString());
+        JSONObject loc = weatherObj.getJSONObject("location");
+        JSONObject temp = weatherObj.getJSONObject("current");
+        JSONObject cond = temp.getJSONObject("condition");
+
+        Label locText = new Label(loc.getString("name") + ", " + loc.getString("country"));
+        locText.setBounds(20, 30, 140, 30);
+        frame.add(locText);
+
+        Label timeText = new Label(loc.getString("localtime"));
+        timeText.setBounds(20, 60, 140, 30);
+        frame.add(timeText);
+
+        Label weatherText = new Label(cond.getString("text"));
+        weatherText.setBounds(20, 90, 140, 30);
+        frame.add(weatherText);
+
+        if (unit == "°C") weatherText.setText(cond.getString("text") + ", " + temp.getDouble("temp_c") + "°C");
+        if (unit == "°F") weatherText.setText(cond.getString("text") + ", " + temp.getDouble("temp_f") + "°F");
+
+        String iconURL = "https:" + cond.getString("icon");
+        try {
+            URL url = new URL(iconURL);
+            Image image = ImageIO.read(url);
+            frame.setIconImage(image);
+            frame.setTitle("Weather App - " + Double.toString(unit == "°C"? temp.getDouble("temp_c"): temp.getDouble("temp_f")));
+        } catch (Exception e) {
+            //Do Nothing if image fails
+        }
+        
+    }
+
+    public static void forecast (StringBuffer rawWeatherData, String unit, Frame frame) throws JSONException {
+        JSONObject weatherObj = new JSONObject(rawWeatherData.toString());
+        JSONObject fore = weatherObj.getJSONObject("forecast");
+        JSONArray day = fore.getJSONArray("forecastday");
+        JSONArray hour = day.getJSONObject(0).getJSONArray("hour");
+
+        Label t1 = new Label("FORECAST:");
+        t1.setBounds(20, 120, 140, 30);
+        frame.add(t1);
+
+        Label t2 = new Label(Utils.getHourly(hour, unit));
+        t2.setBounds(20, 150, 600, 30);
+        frame.add(t2);
+    }
+
+    public static void error (Exception err, Frame f) {
+        Label t1 = new Label("ERROR: " + err.getMessage());
+        t1.setBounds(20, 100, 600, 60);
+        f.add(t1);
+    }
+
+    public static void error (String err, Frame f) {
+        Label t1 = new Label("ERROR: " + err);
+        t1.setBounds(20, 100, 600, 60);
+        f.add(t1);
+    }
+}
+
+class App {    
     App() {  
+        Frame frame = new Frame();
         Label l = new Label("Location:");  
         Button b = new Button("Search");  
         TextField t = new TextField();  
@@ -25,33 +125,55 @@ class App extends Frame {
         b.setBounds(200, 60, 80, 30);  
         c.setBounds(180, 60, 20, 30);
         
-        add(b); 
-        add(l); 
-        add(t);  
+        frame.add(b); 
+        frame.add(l); 
+        frame.add(t);  
 
         c.add("°C");
         c.add("°F");
 
-        add(c);
+        frame.add(c);
    
-        setSize(800, 400);  
+        frame.setSize(800, 400);  
    
-        setTitle("Weather Dashboard");   
-           
-        setLayout(null);   
-        setVisible(true);  
+        frame.setTitle("Weather App");   
+
+        b.addActionListener(new ActionListener() {    
+            public void actionPerformed(ActionEvent e)  {         
+                String unit =  c.getItem(c.getSelectedIndex());    
+                String loc = t.getText();
+                RequestBuilder req = new RequestBuilder();
+
+                try {
+                    StringBuffer dataC = req.getWeatherData(loc, "current");
+                    frame.removeAll();
+                    Print.current(dataC, unit, frame);
+                    StringBuffer dataF = req.getWeatherData(loc, "forecast");
+                    Print.forecast(dataF, unit, frame);
+                } catch (APIException err) {
+                    err.printMessage(frame);
+                } catch (Exception err) {
+                    Print.error(err, frame);
+                }
+            }    
+        }); 
+
+        frame.setLayout(null);   
+        frame.setVisible(true);  
     }
 }    
 
 class RequestBuilder {
-    public void getWeatherData(String location) throws IOException, JSONException {
-        URL url = new URL("http://api.weatherapi.com/v1/current.json?key=" + ClientSecrets.apiKey + "&q=" + location + "&aqi=no");
+    public StringBuffer getWeatherData(String location, String type) throws APIException, IOException, JSONException {
+        URL url = new URL("http://api.weatherapi.com/v1/" + type + ".json?key=" + ClientSecrets.apiKey + "&q=" + location + "&aqi=no");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         con.setConnectTimeout(5000);
         con.setReadTimeout(5000);
 
-        //int status = con.getResponseCode();
+        int status = con.getResponseCode();
+
+        if (status >= 400) throw new APIException("Error", status);
         
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
@@ -62,38 +184,12 @@ class RequestBuilder {
         in.close();
         con.disconnect();
 
-        this.prettify(content);
-    }
-
-    public void prettify(StringBuffer rawWeatherData) throws JSONException {
-        
-        JSONObject weatherObj = new JSONObject(rawWeatherData.toString());
-        JSONObject loc = weatherObj.getJSONObject("location");
-        JSONObject temp = weatherObj.getJSONObject("current");
-        JSONObject cond = temp.getJSONObject("condition");
-
-        System.out.println("Location: " + loc.getString("name") + ", " + loc.getString("country"));
-        System.out.println("Weather: " + temp.getDouble("temp_c") + "C / " + temp.getDouble("temp_f") + "F " + cond.getString("text"));
-        System.out.println("Time: " + loc.getString("localtime"));
+        return content;
     }
 }
 
 class Main {
     public static void main (String args[]) {
-        RequestBuilder req = new RequestBuilder();
-        Scanner sc = new Scanner(System.in);
-
-        App ui = new App();    
-
-        System.out.println("Enter a location: ");
-        String loc = sc.nextLine();
-
-        try {
-            req.getWeatherData(loc);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        sc.close();
+        App ui = new App();   
     }
 }
